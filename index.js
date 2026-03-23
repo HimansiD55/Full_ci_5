d4c532fecd7847f17fb30339baed840b54389920
Himansi Dudhat <139256462+HimansiD55@users.noreply.github.com>
2026-03-23 18:12:08 +0530

    throw error for roll back index.js

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.get('/', (req, res) => {
 app.get('/health', (req, res) => {
   res.json({ version: version });
 });
+/ break it intentionally
+throw new Error("intentional break for testing")
 
+// or just write invalid code
+this is broken code!!!
 // Export app for testing
 module.exports = app;
 
